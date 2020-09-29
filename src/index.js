cbdfaec05058a5e69212c1076411de2dc409a97e
Tav25 <turbin28@gmail.com>
2020-09-29 18:01:51 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    let minNum = "";
+    for (x in array){
+        if (array[x] < minNum ){ minNum = array[x]}
+        // console.log(` ${minNum} ${array[x]}`)
+    }
+    return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxNum = "";
+    for (x in array){
+        if (array[x] > maxNum ){ maxNum = array[x]}
+        // console.log(` ${minNum} ${array[x]}`)
+    }
+    return maxNum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (array = []) {
+    console.log(array)
+    if (array.length === 0 ){ 
+        // console.log("zero")
+        return 0;
+    }
+      let avgNum = 0;
+      for (x in array){
+          avgNum = (array[x] + avgNum);
+  
+      }
+    //   console.log(` ${x}  ${x}`)
+      return avgNum/array.length;
+    //   return 0;
+  }
